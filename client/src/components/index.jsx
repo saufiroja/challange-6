bbbad9f0032229a660b2839791d4e3c3179ed4fc
saufiroja <saufiroja08@gmail.com>
2021-12-10 00:32:24 +0800

    export components

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,2 +1,5 @@
 export { default as NavbarComponents } from "./navbar/NavbarComponents";
-export { default as TableComponents } from "./table/TableComponents";
+export { default as UsersComponents } from "./users/UsersComponents";
+export { default as HomeComponents } from "./home/HomeComponents";
+export { default as CreateUserComponents } from "./createUser/CreateUserComponents";
+export { default as NotFoundComponents } from "./notfound/NotFoundComponents";
