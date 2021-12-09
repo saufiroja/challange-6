b83f80579035a26f469defed3e24b4f6cd35a6f5
saufiroja <saufiroja08@gmail.com>
2021-12-10 00:33:30 +0800

    menambah nav link

--- client/src/components/navbar/NavbarComponents.jsx
+++ client/src/components/navbar/NavbarComponents.jsx
@@ -1,44 +1,43 @@
 import React from "react";
-import {
-  AppBar,
-  Container,
-  Toolbar,
-  Typography,
-  Box,
-  Menu,
-  Button,
-  MenuItem,
-} from "@mui/material";
-
-const pages = ["Home", "About", "Blog"];
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 const NavbarComponents = (props) => {
   return (
-    <AppBar position="relative">
+    <AppBar className={styles.header} position="static">
       <Container maxWidth="xl">
         <Toolbar>
           <Typography variant="h6" sx={{ mr: 2 }}>
-            {props.title}
+            Game
           </Typography>
-          <Box>
-            <Menu>
-              {pages.map((page) => (
-                <MenuItem key={page}>
-                  <Typography>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+          <NavLink
+            className={styles.tabs}
+            to="/"
+            exact
+            variant="h6"
+            sx={{ mr: 2 }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={styles.tabs}
+            to="/users"
+            exact
+            variant="h6"
+            sx={{ mr: 2 }}
+          >
+            All User
+          </NavLink>
+          <NavLink
+            className={styles.tabs}
+            to="/adduser"
+            exact
+            variant="h6"
+            sx={{ mr: 2 }}
+          >
+            Add User
+          </NavLink>
         </Toolbar>
       </Container>
     </AppBar>
