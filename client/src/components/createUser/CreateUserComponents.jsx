a0257e97b88d72ab95d8fc1bc89680be5f8e5212
saufiroja <saufiroja08@gmail.com>
2021-12-10 00:32:41 +0800

    membuat create user components

---
+++ client/src/components/createUser/CreateUserComponents.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CreateUserComponents = () => {
+  return (
+    <div>
+      <h1>Halaman create user</h1>
+    </div>
+  );
+};
+export default CreateUserComponents;
